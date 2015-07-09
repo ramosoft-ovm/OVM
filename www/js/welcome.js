d642cdb8c4a0c5a4e86c28a9cd286e80115ec52b
Diego Acosta <dieaco500@gmail.com>
2015-07-09 15:55:45 -0500

    Validación sala de espera

--- www/js/welcome.js
+++ www/js/welcome.js
@@ -1,23 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-	var userId = localStorage.getItem("userIdLocal");
-	//var userId = 12;
+	//var userId = localStorage.getItem("userIdLocal");
+	var userId = 12;
 	/*Devuelve los últimos inscritos del usuario que esperan por ser agregados a la RED*/
     queryData('USP_VBC_GET_WAITING_ROOM', ['integer', userId], getWaitingRoom);
     function getWaitingRoom(dataSet){
         var rec = dataSet[0];
-        var cont = 0;
+        var cont = 0; //console.log(rec);         
+        
+        //==========VALIDAMOS EL OBJETO QUE NOS DEVUELVE EL QUERYDATA=================//
 
-        for(var idx = 0; idx < dataSet.length; idx++){
-            rec = dataSet[idx];
-            cont = cont+1;
-        }
-
-        if(cont == 0){
+        if(typeof(rec) == 'undefined'){//Si el objeto viene como undefined, significa que no tiene sala de espera
+            //console.log('1');
+            cont = 0;
         	$('table tbody tr:nth-child(4) td a').attr('href', '#');
+        }else if(rec['dataField0'] == 0){//Si el objeto viene con el item dataField0, significa que éste usuario aún no es agregado a la red del patrocinador
+            //console.log('2');
+            cont = 0;
+            $('table tbody tr:nth-child(4) td a').attr('href', '#');
+        }else{//Si el objeto viene cargado con los datos de nuevos RIR'S , significa que el usuario tiene una sala de espera de 'x' miembros
+            //console.log('3');
+            for(var idx = 0; idx < dataSet.length; idx++){
+                rec = dataSet[idx];                
+                cont = cont+1;                
+            }
         }
 
-        console.log(cont);
+        console.log(cont);//Imprimimos contador para pruebas
         $('table tbody tr:nth-child(4) td a').prepend('<span>'+cont+'</span>');
         $('table tbody tr:nth-child(4) td a span').addClass('count'); 
     }
@@ -26,16 +35,22 @@ document.addEventListener('DOMContentLoaded', function(){
     queryData('USP_VBC_GET_MATRIX_VIEWER', ['integer',userId,'integer','0','integer','0','integer','18','integer','1'], getMembersCount);
     function getMembersCount(dataSet){
     	var rec = dataSet[0];
-    	var cont = 0;
+    	var cont = 0; console.log(rec);//imprimimos consulta para pruebas
 
-    	for(var idx = 0; idx < dataSet.length; idx++){
-            rec = dataSet[idx];
-            cont = cont+1;
-        }
-        if(cont == 1){
-            cont = cont-1;
+        //================VALIDAMOS EL OBJETO QUE DEVUELVE EL QUERYDATA===============// 
+
+        if(typeof(rec) == 'undefined'){
             $('table tbody tr:nth-child(3) td a').attr('href', '#');
-        }       
+        }else{
+            for(var idx = 0; idx < dataSet.length; idx++){
+                rec = dataSet[idx];
+                cont = cont+1;
+            }
+            if(cont == 1){//Si el contador es = a 1, significa que el único registro que devuelve el objeto es del mismo usuario, por lo tanto el contador se iguala a 0
+                cont = 0;
+                $('table tbody tr:nth-child(3) td a').attr('href', '#');
+            }
+        }
 
         $('table tbody tr:nth-child(3) td a').prepend('<span>'+cont+'</span>');
         $('table tbody tr:nth-child(3) td a span').addClass('count');
