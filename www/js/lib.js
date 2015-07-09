5203c0632147db485f4a0777bfa651aeab3386dc
juanescru <juane@ramosoft.net>
2015-07-09 11:40:19 -0500

    Charset

--- www/js/lib.js
+++ www/js/lib.js
@@ -4,8 +4,20 @@ function getByURL() {
     var url = location.search.replace("?", "");
         var encontrada = 0;
         while(encontrada == 0) {
-            if (url.indexOf("%20") >= 0) {
+            if (url.indexOf("%20") >= 0 || url.indexOf("á") >= 0 || url.indexOf("é") >= 0 || url.indexOf("í") >= 0 || url.indexOf("ó") >= 0 || url.indexOf("ú") >= 0 || url.indexOf("Á") >= 0 || url.indexOf("É") >= 0 || url.indexOf("Í") >= 0 || url.indexOf("Ó") >= 0 || url.indexOf("Ú") >= 0) {
                 url = url.replace("%20"," ");
+                url = url.replace("%C3%A1","a");
+                url = url.replace("%C3%A9","e");
+                url = url.replace("%C3%AD","i");
+                url = url.replace("%C3%B3","o");
+                url = url.replace("%C3%BA","u");
+                url = url.replace("%C3%81","A;");
+                url = url.replace("%C3%89","E");
+                url = url.replace("%C3%8D","I");
+                url = url.replace("%C3%93","O");
+                url = url.replace("%C3%9A","U");
+                url = url.replace("%C3%91","Ñ");
+                url = url.replace("%C3%B1","ñ");
             } else {
                 encontrada = 1;
             }
