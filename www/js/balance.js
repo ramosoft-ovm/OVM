4305dc372f96533a9ac36341e23fdb095b139c81
Diego Acosta <dieaco500@gmail.com>
2015-07-10 00:42:20 -0500

    Función para validar rangos de fechas

--- www/js/balance.js
+++ www/js/balance.js
@@ -96,3 +96,91 @@ document.addEventListener('DOMContentLoaded', function() {
     /******** Fin declaración de Eventos *********/
     ///////////////////////////////////////////////
 });
+
+
+
+
+
+
+/*function RangosDeFecha(fechaIni, fechaFin) {
+        //Declaración de variables
+        var that = this;
+        var date = new Date();
+        that.rangoInicial = '';
+        that.rangoFinal = '';
+
+        //Verifica si existen parametros
+        if (typeof fechaIni != 'undefined' && typeof fechaFin != 'undefined') {
+            this.fechaIni = fechaIni.split('-');
+            this.fechaFin = fechaFin.split('-');
+            //Fecha inicial
+            that.diaIni = this.fechaIni[2];
+            that.mesIni = this.fechaIni[1];
+            that.añoIni = this.fechaIni[0];
+            //Fecha Final
+            that.diaFin = this.fechaFin[2];
+            that.mesFin = this.fechaFin[1];
+            that.añoFin = this.fechaFin[0];
+        }
+
+        that.getRangoActual = function() {
+            var mes = date.getMonth() + 1;
+            var año = date.getFullYear();
+            mes = that.corregir(mes);
+            that.rangoInicial = año + '-' + mes + '-' + '01'
+            that.rangoFinal = año + '-' + mes + '-' + '31';
+        }
+        that.getRangoAnterior = function() {
+            if (that.mesIni <= 1) {
+                that.mesIni = 12
+                that.añoIni = parseInt(that.añoIni) - 1;
+            }
+            else {
+                that.mesIni = parseInt(that.mesIni) - 1;
+            }
+            if (that.mesFin <= 1) {
+                that.mesFin = 12
+                that.añoFin = parseInt(that.añoFin) - 1;
+            }
+            else {
+                that.mesFin = parseInt(that.mesFin) - 1;
+            }
+            that.mesIni = that.corregir(that.mesIni);
+            that.rangoInicial = that.añoIni + '-' + that.mesIni + '-' + that.diaIni;
+            that.mesFin = that.corregir(that.mesFin);
+            that.rangoFinal = that.añoFin + '-' + that.mesFin + '-' + that.diaFin;
+        }
+        that.getRangoSiguiente = function() {
+            if (that.mesIni > 11) {
+                that.mesIni = 1;
+                that.añoIni = that.añoIni + 1;
+            }
+            else {
+                that.mesIni = parseInt(that.mesIni) + 1;
+            }
+            if (that.mesFin > 11) {
+                that.mesFin = 1;
+                that.añoFin = parseInt(that.añoFin) + 1;
+            }
+            else {
+                that.mesFin = parseInt(that.mesFin) + 1;
+            }
+            that.mesIni = that.corregir(that.mesIni);
+            that.rangoInicial = that.añoIni + '-' + that.mesIni + '-' + that.diaIni;
+            that.mesFin = that.corregir(that.mesFin);
+            that.rangoFinal = that.añoFin + '-' + that.mesFin + '-' + that.diaFin;
+        }
+        that.corregir = function(mes) {
+            this.mes = parseInt(mes);
+            if (this.mes < 10){
+                this.mes = '0' + this.mes;
+            }
+            return this.mes;
+        }
+        that.añoBiciesto = function(año) {
+            if ((((año%100)!=0)&&((año%4)==0))||((año%400)==0)){
+                return true;
+            }
+            return false
+        }
+    }*/
