6b679937b84bf14e28d5736d832adafe1d8a71f6
Diego Acosta <dieaco500@gmail.com>
2015-07-13 19:16:48 -0500

    Función get_date_range para sql server

--- www/js/classDates.js
+++ www/js/classDates.js
@@ -118,7 +118,7 @@ function RangosDeFecha(fechaIni, fechaFin,txtAnterior,txtSiguiente) {
         return dias[parseInt(mes)-1]
     }
     var procedimiento = function(dataSet) {
-        var rec = dataSet[0];
+        var rec = dataSet[0];console.log(rec);
         var result = rec['startDate'];
         var fecha = rec['startDate'];
         var dia = '';
@@ -187,3 +187,106 @@ function RangosDeFecha(fechaIni, fechaFin,txtAnterior,txtSiguiente) {
         that.txtFechaFinal.value = that.rangoFinal;
     }
 }
+
+
+
+/*SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:      <DIEGO, ACOSTA>
+-- Create date: <13 DE JULIO DE 2016>
+-- Description: <CALCULA RANGO DE FECHAS SEGÚN EL MES Y EL AÑO>
+-- =============================================
+CREATE FUNCTION [dbo].[FN_ACOSTA_GET_DATE_RANGE](@PS_INI_DATE nvarchar(20))
+RETURNS @INFO TABLE(
+    INI_DATE datetime,
+    FIN_DATE datetime,
+    TOTAL_DAYS int
+) 
+AS
+BEGIN
+    --SE DECLARAN VARIABLES LOCALES A CALCULAR--
+    DECLARE
+        @LD_INI_DATE nvarchar(20),
+        @LD_INI_DATE2 datetime,
+        @LD_FIN_DATE datetime,
+        @LN_DAYS int
+
+
+    --SÍ LA CADENA CONTIENE ESPACIO SIGNIFICA QUE ES DEL TIPO '26 AUG 2015' Y HAY QUE CONVERTIRLA A '2015-08-26'--
+    IF NOT SUBSTRING(@PS_INI_DATE,3,1) = ' '
+        BEGIN
+            --SE CALCULA FECHA INICIAL DEL MES--
+            SET @LD_INI_DATE = SUBSTRING(@PS_INI_DATE,0,5) + SUBSTRING(@PS_INI_DATE,6,2) + '01'
+
+            --CONVIERTE LA CADENA EN UN DATETIME--
+            SET @LD_INI_DATE2 = CONVERT(datetime, @LD_INI_DATE)
+
+            --SE ASIGNA 1 MES COMO DIFERENCIA --
+            SET @LD_FIN_DATE = DATEADD(mm,1,@LD_INI_DATE2)
+
+            --SE CALCULAN LOS DÍAS QUE TIENE EL MES SOLICITADO--
+            SET @LN_DAYS = DATEDIFF(dd,@LD_INI_DATE2,@LD_FIN_DATE)
+
+            --SE ASIGNA LA FECHA FINAL DEL RANGO TOMANDO EN CUENTA EL MES, EL AÑO Y LOS DÍAS QUE TIENE ESE MES--
+            SET @LD_FIN_DATE = DATEADD(mi,-1,DATEADD(dd,(@LN_DAYS),@LD_INI_DATE2));--ultimo dia
+
+
+            INSERT INTO @INFO(INI_DATE, FIN_DATE, TOTAL_DAYS)
+                    VALUES(@LD_INI_DATE2, @LD_FIN_DATE, @LN_DAYS)
+        END
+    ELSE
+        BEGIN
+            DECLARE
+                @LS_DAY nvarchar(4),
+                @LS_MONTH nvarchar(15),
+                @LS_YEAR nvarchar(6)
+
+            --SES ESTABLECE EL DÍA DE LA FECHA--
+            SET @LS_DAY = '01'
+            --SE ESTALEBE EL AÑO DE LA FECHA--
+            SET @LS_YEAR = SUBSTRING(@PS_INI_DATE,8,12)         
+            --SE ESTABLECE EL MES DE LA FECHA DEL TIPO 'AUG' Y SE CONVIERTE AL TIPO '08'--
+            SET @LS_MONTH = (
+                CASE 
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Jan' THEN '01'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Feb' THEN '02'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Mar' THEN '03'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Apr' THEN '04'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'May' THEN '05'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Jun' THEN '06'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Jul' THEN '07'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Aug' THEN '08'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Sep' THEN '09'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Oct' THEN '10'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Nov' THEN '11'
+                    WHEN SUBSTRING(@PS_INI_DATE,4,7) = 'Dic' THEN '12'
+                END         
+            )
+
+            --SE CALCULA FECHA INICIAL DEL MES--
+            SET @LD_INI_DATE = @LS_YEAR + @LS_MONTH + @LS_DAY
+
+            --CONVIERTE LA CADENA EN UN DATETIME--
+            SET @LD_INI_DATE2 = CONVERT(datetime, @LD_INI_DATE)
+
+            --SE ASIGNA 1 MES COMO DIFERENCIA --
+            SET @LD_FIN_DATE = DATEADD(mm,1,@LD_INI_DATE2)
+
+            --SE CALCULAN LOS DÍAS QUE TIENE EL MES SOLICITADO--
+            SET @LN_DAYS = DATEDIFF(dd,@LD_INI_DATE2,@LD_FIN_DATE)
+
+            --SE ASIGNA LA FECHA FINAL DEL RANGO TOMANDO EN CUENTA EL MES, EL AÑO Y LOS DÍAS QUE TIENE ESE MES--
+            SET @LD_FIN_DATE = DATEADD(mi,-1,DATEADD(dd,(@LN_DAYS),@LD_INI_DATE2));--ultimo dia
+
+
+            INSERT INTO @INFO(INI_DATE, FIN_DATE, TOTAL_DAYS)
+                    VALUES(@LD_INI_DATE2, @LD_FIN_DATE, @LN_DAYS)
+
+        END
+    
+
+    RETURN
+END*/
